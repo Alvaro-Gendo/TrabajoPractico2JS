87e37e72b5ca8b2a0fcbbf54ee491586fc627cb1
Alvaro <zhuone@hotmail.com>
2022-07-09 18:35:21 -0300

    ejercicio n1 terminado

---
+++ js/Ejercicio1.js
@@ -0,0 +1,10 @@
+let edad = parseInt(prompt("Ingrese su edad"));
+
+
+if(edad >= 18){
+    document.write("Tinese edad para conducir");
+}else if(edad < 18){
+    document.write("No tines edad para conducir");
+}else{
+    document.write("No ingresaste un nummero valido")
+}
