a9d306a93dac95b540f73aec26503b8e71c7e6d2
Alvaro <zhuone@hotmail.com>
2022-07-10 18:38:40 -0300

    resuelto problema de piramide, falta probelma de seleccion de numero

--- js/Ejercicio6.js
+++ js/Ejercicio6.js
@@ -2,5 +2,6 @@ let numero = 1
 
 for(numero = 1; numero <= 30; numero++){
     for(let repe = 0; repe < numero; repe++)
-       document.write("<br>" + numero)
+       document.write(numero)
+       document.write("<br>")
 }
--- js/Ejercicio7.js
+++ js/Ejercicio7.js
@@ -2,10 +2,11 @@ let numero = 1
 
 numero = parseInt(prompt("Ingrese un número del 1 al 50"))
 
-if(nuermo >= 0 && nuermo <= 50){
+if(numero >= 0 && numero <= 50){
     for(numero = 50; numero >= 1; numero--){
         for(repe = 0; repe < numero; repe++)
-        document.write("<br>" + numero)
+        document.write(numero)
+        document.write("<br>")
     }
 }else{
     alert("Número incorrecto")
