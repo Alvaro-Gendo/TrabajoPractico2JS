822a13cab3286322b98b6d930fa254b5f6671886
Alvaro <zhuone@hotmail.com>
2022-07-13 20:20:23 -0300

    problemas con ejercicio 7

--- js/Ejercicio7.js
+++ js/Ejercicio7.js
@@ -1,13 +1,9 @@
 let numero = 1
 
-numero = parseInt(prompt("Ingrese un número del 1 al 50"))
-
-if(numero >= 0 && numero <= 50){
-    for(numero = 50; numero >= 1; numero--){
-        for(repe = 0; repe < numero; repe++)
-        document.write(numero)
-        document.write("<br>")
-    }
-}else{
-    alert("Número incorrecto")
+for(numero = 1; numero <= 30; numero++){
+    for(let repe = 0; repe < numero; repe++)
+       document.write(numero)
+       document.write("<br>")
 }
+
+
