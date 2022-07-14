f843105a7f539edf3b40c64dc5c65406f6c2327b
Alvaro <zhuone@hotmail.com>
2022-07-14 20:49:12 -0300

    aceptando el conflicto en el ejercicio 7

--- js/Ejercicio7.js
+++ js/Ejercicio7.js
@@ -1,5 +1,4 @@
 let numero = 1
-<<<<<<< HEAD
 let incremento;
 let repe;
 numero = parseInt(prompt("Ingrese un número del 1 al 50"))
@@ -15,13 +14,3 @@ if(numero > 0 && numero <= 50){
 }
 
 //Posible bucle inf. no logro arreglarlo...
-=======
-
-for(numero = 1; numero <= 30; numero++){
-    for(let repe = 0; repe < numero; repe++)
-       document.write(numero)
-       document.write("<br>")
-}
-
-
->>>>>>> ejercicio8
