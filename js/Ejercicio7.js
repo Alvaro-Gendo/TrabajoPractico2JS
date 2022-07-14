d6e7c9b6e9830d53773bdc321ea93c2e8bee332f
Alvaro <zhuone@hotmail.com>
2022-07-14 20:45:23 -0300

    problemas con el ejercicio 7

--- js/Ejercicio7.js
+++ js/Ejercicio7.js
@@ -1,13 +1,16 @@
 let numero = 1
-
+let incremento;
+let repe;
 numero = parseInt(prompt("Ingrese un número del 1 al 50"))
 
-if(numero >= 0 && numero <= 50){
-    for(numero = 50; numero >= 1; numero--){
-        for(repe = 0; repe < numero; repe++)
+if(numero > 0 && numero <= 50){
+    for(numero = 1; numero >= 50; numero--){
+        for(repe = incremento; repe >= incremento; repe--)
         document.write(numero)
         document.write("<br>")
     }
 }else{
     alert("Número incorrecto")
 }
+
+//Posible bucle inf. no logro arreglarlo...
